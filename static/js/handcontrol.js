c76442e888f1e114a475edc086c1ebec821af435
deezusdyse <32604268+deezusdyse@users.noreply.github.com>
2018-10-17 22:51:40 -0400

    cleanup

--- static/js/handcontrol.js
+++ static/js/handcontrol.js
@@ -102,9 +102,6 @@ function scorebar() {
 }
 
 
-
-//!!the ball is printing balls that haven't appeared yet.
-
 // Bricks
 var brickHeight = 25; 
 var brickWidth = 50;
@@ -120,7 +117,6 @@ var brickOffsetLeft = (canvas.width-widthSum - paddingSum)/2.0;
 paddle = new Paddle(x_coord, canvas.height-paddleHeight-offset, paddleWidth, paddleHeight);
 
 ws = new WebSocket(socketurl);
-//console.log("attempting socket connection ");
 
 function connectSocket() {
     ws.onopen = function () {
@@ -143,7 +139,7 @@ function connectSocket() {
 
         setTimeout(function () {
             connectSocket();
-            //console.log("lengthof of paddleBodiesHoder", paddleBodies.length);
+           
         }, 3000)
     };
 }
@@ -158,7 +154,7 @@ function getXCord(data) {
         currentMap.set(point.id_label, { timestamp: Date.now() })
         if (!paddleBodies.has(point.id_label)) {
             paddleMap.set(point.id, point)
-            //addPaddle(point.id_label)
+           
         }
         box = point.box;
         top = box[0] * windowHeight;
@@ -167,49 +163,37 @@ function getXCord(data) {
         height = Math.abs(box[2] - (box[0])) * windowHeight;
         midx = point.box_center[0];
         
-        //paddle.update(midx);
-		//console.log("midx:" + midx);
-		// assume max x is 290
-		x_coord = (midx * canvas.width/290) - 180;
+	x_coord = (midx * canvas.width/290) - 180;
     })
 }
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Older browsers might not implement mediaDevices at all, so we set an empty object first
 if (navigator.mediaDevices === undefined) {
   navigator.mediaDevices = {};
 }
-//Some browsers partially implement mediaDevices. We can't just assign an object
-//with getUserMedia as it would overwrite existing properties.
-//Here, we will just add the getUserMedia property if it's missing.
+
 if (navigator.mediaDevices.getUserMedia === undefined) {
   navigator.mediaDevices.getUserMedia = function(constraints) {
 
-    // First get ahold of the legacy getUserMedia, if present
+
     var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-    // Some browsers just don't implement it - return a rejected promise with an error
-    // to keep a consistent interface
     if (!getUserMedia) {
       return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
     }
 
-    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    
     return new Promise(function(resolve, reject) {
       getUserMedia.call(navigator, constraints, resolve, reject);
     });
   }
 }
 
-// set up forked web audio context, for multiple browsers
-// window. is needed otherwise Safari explodes
 
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var source;
 var stream;
 
-//set up the different audio nodes we will use for the app
 
 var analyser = audioCtx.createAnalyser();
 analyser.minDecibels = -90;
@@ -243,18 +227,15 @@ ajaxRequest.onload = function() {
       convolver.buffer = buffer;
     }, function(e){ console.log("Error with decoding audio data" + e.err);});
 
-  //soundSource.connect(audioCtx.destination);
-  //soundSource.loop = true;
-  //soundSource.start();
+
 };
 
 ajaxRequest.send();
 
-// set up canvas context for visualizer
+
 
 var drawVisual;
 
-//main block for doing the audio recording
 
 if (navigator.mediaDevices.getUserMedia) {
    console.log('getUserMedia supported.');
@@ -269,7 +250,7 @@ if (navigator.mediaDevices.getUserMedia) {
            biquadFilter.connect(gainNode);
            convolver.connect(gainNode);
            gainNode.connect(audioCtx.destination);
-           //generateBlocks();
+          
 
       })
       .catch( function(err) { console.log('The following gUM error occured: ' + err);})
@@ -277,15 +258,13 @@ if (navigator.mediaDevices.getUserMedia) {
    console.log('getUserMedia not supported on your browser!');
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////
+
 
 var index = 0;
 var brickRow = [];
 var brickRowArrays = [];
 var bricks = [];
 
-//need to initialize the first row.   5 blocks. 
-//later change according to screen size
 brickRow[0] = {numBricks: 5, status: 1};	  
 brickRowArrays[0] = [1,3,4,12,13];
 bricks[0] = [];
@@ -309,36 +288,34 @@ function storeBricks() {
 			var counter = index / 100;
     		barHeight = Math.max.apply(Math, dataArrayAlt);
 
-    		var heightPercent = barHeight/235.00;      ///235 is max height
+    		var heightPercent = barHeight/235.00;      
     		var numColumns = Math.round(brickColumns * heightPercent);
      		brickRow[counter] = {numBricks: numColumns};	  
      		var arr = [];
      	
      		try {
-				var intendedLength = brickRow[counter].numBricks;
-				while(arr.length < brickRow[counter].numBricks){ //brickRow[r].numBricks is undefined
-    				var randomnumber = Math.floor(Math.random()*brickColumns); //goes from [0: numBricks-1] //but this updates random everytime run.
-    				if(arr.indexOf(randomnumber) > -1) continue;
-   					arr[arr.length] = randomnumber; 
-   					brickRowArrays[counter] = arr;       		
+			var intendedLength = brickRow[counter].numBricks;
+			while(arr.length < brickRow[counter].numBricks){ 
+			var randomnumber = Math.floor(Math.random()*brickColumns); 
+			if(arr.indexOf(randomnumber) > -1) continue;
+				arr[arr.length] = randomnumber; 
+				brickRowArrays[counter] = arr;       		
 				} 
 			}
 			catch(e) {
 				console.log(e);
 			} 		
 			bricks[counter] = [];
-			//store bricks information
-		
-		
-			///put this in the print Bricks function to update the image every time. 
+			
+
 			for ( i = 0; i < brickRowArrays[counter].length; i ++) {
 				var brickXpos = (brickRowArrays[counter][i]*(brickWidth+brickPadding))+brickOffsetLeft;
-				var brickYpos = (brickOffsetTop - counter * (brickHeight+brickPadding)); //instead of going from top bottom, need to bottom top
+				var brickYpos = (brickOffsetTop - counter * (brickHeight+brickPadding)); 
 				if (counter < brickRows) {
-					bricks[counter][i] = {x:brickXpos, y:brickYpos, status:1 }; //counter is row, i is column  
+					bricks[counter][i] = {x:brickXpos, y:brickYpos, status:1 }; 
 				}
 				else {
-					bricks[counter][i] = {x:brickXpos, y:brickYpos, status:2 }; ///stored for future use
+					bricks[counter][i] = {x:brickXpos, y:brickYpos, status:2 }; 
 				}
 			}		
 		}
@@ -353,32 +330,21 @@ function updateBricks() {
 			count++;
 		}	
 	}
-	if (count == bricks[0].length) {  ///I dont understand how this is right though
-		//pop the first element, 
-		//console.log('weird');
-		//console.log(bricks[brickRows-1]);		
-		brickRowArrays.shift();  // maybe need to delete the brickRows'th row // maybe splicing //think about order. based on difficulty
+	if (count == bricks[0].length) {  		
+		brickRowArrays.shift(); 
 		for (i = 0; i < bricks[brickRows-1].length; i ++){
 			b = bricks[brickRows-1][i];
 			b.status = 1;
-			//need to update the location as well//also might need alternative way 
-		}; ///stored for future use
+		};
 		
 	}
 }
 
 
-//!!  how about only print when a row is done receiving volume input 
+
 function printBricks() {
 	for (r = 0; r < brickRows; r++) {
-// 		console.log('bricks');
-// 		console.log(bricks);
-// 		console.log('r');
-// 		console.log(r);
-// 		console.log('bricks[r]'); 
-// 		console.log(bricks[r]);
-		//seems to be out of bounds bricks[r] undefined when r = brickRows -1 
-		for (c = 0; c < bricks[r].length; c++){ //cannot read proprety length of undefined
+		for (c = 0; c < bricks[r].length; c++){ 
 			var b = bricks[r][c]; 
 			if (b.status == 1) {   
         		ctx.beginPath();
@@ -409,7 +375,6 @@ function bricksCollision() {
 }
 
 // Function that creates the ball
-
 function ball() {
     ctx.beginPath();
     ctx.arc(xPos, yPos, ballRadius, 0, Math.PI*2);
@@ -421,7 +386,6 @@ function ball() {
 
 
 // Uncategorized variables
-
 var score = 0;
 var ballColor = randomColor();
 
@@ -440,7 +404,6 @@ function Paddle(x,y, timestamp, id) {
 	this.id = id;
 	
 	this.draw = function() {
-		//console.log("drawing x");
 		ctx.beginPath();
    		ctx.rect(this.x, this.y, paddleWidth, paddleHeight);
     	ctx.fillStyle = "#3498db";
@@ -449,19 +412,13 @@ function Paddle(x,y, timestamp, id) {
 	}
 	
 	this.update = function(x_update) {
-		//this.x = getXCord(data);
-		///paddle = new Paddle(getXCord(data), y,.)			
-		//this.x  = this.x_update;
 		this.x = x_update;
-		//console.log("updating x");
 		this.draw();
 		}
 }
 
 function animate() {
-	//console.log("animate outside");
 	if (!pauseGame) {
-		//console.log("animate inside");
 		requestAnimationFrame(animate);
 		ctx.clearRect(0, 0, innerWidth, innerHeight);
 		paddle.update(x_coord);
@@ -471,7 +428,6 @@ function animate() {
 		updateBricks();
 		printBricks();
 		bricksCollision();
-		//this.update();
 		paddle.update(x_coord);
     	if(xPos + xMove > canvas.width-ballRadius || xPos + xMove < ballRadius) {
      	   xMove = -xMove;
@@ -484,8 +440,6 @@ function animate() {
             	yMove = -yMove;
         	}
         	else {
-            	//alert("Game over, try again!");
-            	//document.location.reload();
         	}
    			}
     	if(paddleRight && x_coord < canvas.width-paddleWidth) {
@@ -503,7 +457,5 @@ addUI();
 animate();
 
 
-
-//use musical patterns to create the blcoks
 
 
